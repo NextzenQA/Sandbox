315e2c19d7218c7cb0bd6f26412005c225045bb2
NextzenQA <132110613+NextzenQA@users.noreply.github.com>
2023-05-09 20:51:50 -0400

    Add files via upload

---
+++ AmarTestProject/AmarTestProject/Features/AmazonSearch.feature
@@ -0,0 +1,23 @@
+﻿Feature: Amazon Search
+
+
+Scenario: Validate amazon search within a department - Mens
+Given I am amazon search
+When I search for 'shoes' in 'search-alias=fashion-mens's department
+Then I should the shoes in "Men's Fashion" in results
+
+Scenario: Validate amazon search within a department - Women
+Given I am amazon search
+When I search for 'belts' in 'search-alias=fashion-womens's department
+Then I should the shoes in "Women's Fashion" in results
+
+
+#Men, shoes - test data
+#Women, belts - test data
+
+# should not be more than 5 lines . 3 - good, 4 -ok 5 limit
+#B - Business Language - do not use words like click, enter
+#R - Real Data - do not use test specific data
+#I - Intention Revealing - DO NOT DO THIS - decribing application mechanics - How to , what are you expecting from the sye
+#E - Essential Information
+#F - Focus
---
+++ AmarTestProject/AmarTestProject/StepDefinitions/AmazonSearchStepDefinitions.cs
@@ -0,0 +1,59 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using TechTalk.SpecFlow;
+
+namespace AmarTestProject.StepDefinitions
+{
+    [Binding]
+    public class AmazonSearchStepDefinitions
+    {
+        IWebDriver driver;
+
+        [Given(@"I am amazon search")]
+        public void GivenIAmAmazonSearch()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.amazon.com");
+        }
+
+        [When(@"I search for '([^']*)' in '([^']*)'s department")]
+        public void WhenISearchForInSDepartment(string searchKey, string department)
+        {
+
+
+            //Find department dropdown
+            var departmentDropdown = driver.FindElement(By.XPath("//*[@id='searchDropdownBox']"));
+            //Select user input department
+            SelectElement myFirstDropdownAutomaiton = new SelectElement(departmentDropdown);
+            myFirstDropdownAutomaiton.SelectByValue(department);
+
+            //Dropdown automaiton
+            //1. find select element using normal locator technique mechanism
+            //2. create a SelectElement class object for the above element
+            //3. perform the automaiton action using the object you created on SelectElement class
+
+            //Find Search field
+            var searchField = driver.FindElement(By.XPath("//*[@id='twotabsearchtextbox']"));
+            //Enter your searchKey into search field
+            searchField.SendKeys(searchKey);
+
+            //Find search icon
+            var searchIcon = driver.FindElement(By.XPath("//*[@id='nav-search-submit-button']"));
+            //click on search icon
+            searchIcon.Click();
+        }
+
+
+        [Then(@"I should the shoes in ""([^""]*)"" in results")]
+        public void ThenIShouldTheShoesInInResults(string searchResultsKey)
+        {
+            var departmentInSearchResults = driver.FindElement(By.XPath($"//span[@class='a-list-item']/span[text() =\"{searchResultsKey}\"]"));
+            Assert.IsTrue(departmentInSearchResults.Displayed, "Not able to find search key in results");
+        }
+
+       
+    }
+}
