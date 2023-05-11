98f14c8c4e624ba0c9be05a3ac3813308a66b652
NextzenQA <132110613+NextzenQA@users.noreply.github.com>
2023-05-10 20:59:19 -0400

    Add files via upload

---
+++ AmarTestProject/AmarTestProject/Support/ObjectRepository.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmarTestProject.Support
+{
+    public class ObjectRepository
+    {
+        public static IWebDriver Driver;
+    }
+}
