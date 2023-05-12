3c3fa97863cf2b2d4c1c6ed85a83e070f2080fa2
NextzenQA <132110613+NextzenQA@users.noreply.github.com>
2023-05-12 08:47:27 -0400

    Add files via upload

---
+++ AmarTestProject/AmarTestProject/Hooks/ScenarioHookscs.cs
@@ -0,0 +1,27 @@
+﻿using AmarTestProject.Support;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+
+namespace AmarTestProject.Hooks
+{
+    [Binding]
+    public sealed class ScenarioHookscs
+    {
+        // For additional details on SpecFlow hooks see http://go.specflow.org/doc-hooks
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            ObjectRepository.Driver = new ChromeDriver();
+            //Implicit Wait imlimentation
+            ObjectRepository.Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            ObjectRepository.Driver.Quit();
+        }
+    }
+}
---
+++ AmarTestProject/AmarTestProject/StepDefinitions/AmazonSearchStepDefinitions.cs
@@ -0,0 +1,75 @@
+using AmarTestProject.Support;
+using FluentAssertions.Specialized;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using TechTalk.SpecFlow;
+
+namespace AmarTestProject.StepDefinitions
+{
+    [Binding]
+    public class AmazonSearchStepDefinitions
+    {
+        
+
+        [Given(@"I am amazon search")]
+        public void GivenIAmAmazonSearch()
+        {
+            ObjectRepository.Driver.Navigate().GoToUrl("https://www.amazon.com");
+        }
+
+        [When(@"I search for '([^']*)' in '([^']*)'s department")]
+        public void WhenISearchForInSDepartment(string searchKey, string department)
+        {
+
+           
+            //Find department dropdown
+            var departmentDropdown = ObjectRepository.Driver.FindElement(By.XPath("//*[@id='searchDropdownBox']"));
+            //Select user input department
+            SelectElement myFirstDropdownAutomaiton = new SelectElement(departmentDropdown);
+            myFirstDropdownAutomaiton.SelectByText(department);
+
+            
+            //Explicit wait implementation
+            WebDriverWait wait = new WebDriverWait(ObjectRepository.Driver, TimeSpan.FromSeconds(120));
+            wait.Until(
+                karrem => karrem.FindElement(By.XPath("")).Text.Equals("Sai")
+                
+                );
+
+            
+
+            //wait.until(ExpectedConditions.Visbility...)
+
+
+            //Dropdown automaiton
+            //1. find select element using normal locator technique mechanism
+            //2. create a SelectElement class object for the above element
+            //3. perform the automaiton action using the object you created on SelectElement class
+
+            //Find Search field
+            var searchField = ObjectRepository.Driver.FindElement(By.XPath("//*[@id='twotabsearchtextbox']"));
+            //Enter your searchKey into search field
+            searchField.SendKeys(searchKey);
+
+            
+
+            //Find search icon
+            var searchIcon = ObjectRepository.Driver.FindElement(By.XPath("//*[@id='nav-search-submit-button']"));
+            //click on search icon
+            searchIcon.Click();
+        }
+
+
+        [Then(@"I should the shoes in ""([^""]*)"" in results")]
+        public void ThenIShouldTheShoesInInResults(string searchResultsKey)
+        {
+            var departmentInSearchResults = ObjectRepository.Driver.FindElement(By.XPath($"//span[@class='a-list-item']/span[text() =\"{searchResultsKey}\"]"));
+            Assert.IsTrue(departmentInSearchResults.Displayed, "Not able to find search key in results");
+        }
+
+       
+    }
+}
