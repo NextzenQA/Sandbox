461e2d83d5f3bad97d1a24a240303035770a7bfb
pavan7744-git <ch.pavankalyan1212@gmail.com>
2023-07-07 16:49:50 -0400

    calculator

---
+++ PavanUnitTestCases/UnitTest/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PavanUnitTestCases.UnitTest
+{
+    public class Calculator
+    {
+        // In this Project
+        // IDE Integrated Development Environment - Visual Studios
+        // C#
+        // This code is written by developer 
+
+        public int AddTwoNumbers(int a, int b)
+        {
+            return a + b;
+        }
+        public int MultiplicationTwoNumbers(int a, int b)
+        {
+            return a * b;
+        }
+
+    }
+}
