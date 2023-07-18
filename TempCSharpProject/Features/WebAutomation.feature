55acefe8074165f7749da2c7e48f37cf78767715
lakshmi <lakshmi.koraganji88@gmail.com>
2023-07-18 17:31:48 -0400

    C# project, Amazon search

---
+++ TempCSharpProject/Features/WebAutomation.feature
@@ -0,0 +1,9 @@
+﻿Feature: WebAutomation
+
+A short summary of the feature
+
+@tag1
+Scenario: Validate amazon search functionality 
+	Given I am on amazon search page
+	When I search for electronics
+	Then I should see electronics results
---
+++ TempCSharpProject/StepDefinitions/BackendValidationsStepDefinitions.cs
@@ -0,0 +1,27 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace TempCSharpProject.StepDefinitions
+{
+    [Binding]
+    public class BackendValidationsStepDefinitions
+    {
+        [Given(@"I have an updated employee schedule")]
+        public void GivenIHaveAnUpdatedEmployeeSchedule()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I run so amd so job")]
+        public void WhenIRunSoAmdSoJob()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"my database table should be updated with so and so")]
+        public void ThenMyDatabaseTableShouldBeUpdatedWithSoAndSo()
+        {
+            throw new PendingStepException();
+        }
+    }
+}
