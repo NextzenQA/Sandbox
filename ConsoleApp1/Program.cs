c5f3efbc6c803ace3e206f323d17b73d4f9173a3
lakshmi <lakshmi.koraganji88@gmail.com>
2023-07-20 17:28:51 -0400

    Lakshmi Switch case example

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿BevarageDecision:
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Do you want to order a Bevarage," +
+    "Type 'C' for Coffee, Type 'T' for Tea");
+string userDecision = Console.ReadLine();
+/*
+if (userDecision.Equals("C"))
+{
+    Console.WriteLine("What Size ? 's' = small, m = medium, 'l' = large ");
+}
+else if (userDecision.Equals("T")){
+    Console.WriteLine("What Size ? 's' = small, m = medium, 'l' = large ");
+
+}
+else
+{
+    Console.WriteLine("Invalid Input");
+    goto Label;
+}
+
+*/
+if (userDecision.Equals("C"))
+{
+    CoffeeSize:
+    Console.WriteLine("What Size ? 's' = small, m = medium, 'l' = large ");
+    string sizeOfCoffee = Console.ReadLine();
+
+    switch (sizeOfCoffee)
+    {
+        case "s":
+            Console.WriteLine("Small coffee is 1 dollar");
+            break;
+        case "m":
+            Console.WriteLine("Small coffee is 2 dollar");
+            break;
+        case "l":
+            Console.WriteLine("Small coffee is 3 dollar");
+            break;
+        default:
+            Console.WriteLine("Invalid selection");
+            goto CoffeeSize;
+            break;
+    }
+}
+else if (userDecision.Equals("T"))
+{
+    Console.WriteLine("What Size ? 's' = small, m = medium, 'l' = large ");
+
+}
+else
+{
+    Console.WriteLine("Invalid Input");
+    goto BevarageDecision;
+}
+
